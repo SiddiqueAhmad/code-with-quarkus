ac56eb88fb9f9d4eebd77c6b77d68db66252a511
Siddique Ahmad <siddique.ahmad@gmail.com>
2020-12-19 11:12:54 +0500

    added completion stage option

--- src/main/java/me/escoffier/ApiListConsumer.java
+++ src/main/java/me/escoffier/ApiListConsumer.java
@@ -6,9 +6,11 @@ package me.escoffier;
         import devdbserver1.dev_16_t_cloud_clinic.api_list.Value;
         import org.eclipse.microprofile.reactive.messaging.Incoming;
         import org.eclipse.microprofile.reactive.messaging.Acknowledgment;
+        import org.eclipse.microprofile.reactive.messaging.Message;
         import org.jboss.logging.Logger;
 
         import javax.enterprise.context.ApplicationScoped;
+        import java.util.concurrent.CompletionStage;
 
 @ApplicationScoped
 public class ApiListConsumer {
@@ -16,21 +18,26 @@ public class ApiListConsumer {
     private static final Logger LOGGER =
             Logger.getLogger("MovieConsumer");
 
+//    @Incoming("api-list-from-kafka")
+//    @Acknowledgment(Acknowledgment.Strategy.POST_PROCESSING)
+//    public void receive(Envelope envelope) {
+//        if(envelope != null){
+//            LOGGER.info("Received envelope");
+//            if(envelope.getAfter() != null)
+//            LOGGER.infof("envelope source db %s", envelope.getAfter().getName());
+//        }
+//
+//        else
+//            LOGGER.info("Received null envelope");
+//
+//    }
+
     @Incoming("api-list-from-kafka")
-    public void receive(Envelope envelope) {
-        if(envelope != null){
-            LOGGER.info("Received envelope");
-            if(envelope.getAfter() != null)
-            LOGGER.infof("envelope source db %s", envelope.getAfter().getName());
-        }
+    public CompletionStage<Void> consume(Message<Envelope> envelopeMessage) {
+        // process your price.
 
-        else
-            LOGGER.info("Received null envelope");
-
-
-
-
-
+        // Acknowledge the incoming message (commit the offset)
+        return envelopeMessage.ack();
     }
 
 
